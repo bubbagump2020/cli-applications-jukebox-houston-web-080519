f5da44567a97642727c4f755229bd767e825a81d
Kevin Bagnall <bubbagump2020@gmail.com>
2019-07-26 19:00:21 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,1 +1,66 @@
 # Add your code here
+songs = [
+  "Phoenix - 1901",
+  "Tokyo Police Club - Wait Up",
+  "Sufjan Stevens - Too Much",
+  "The Naked and the Famous - Young Blood",
+  "(Far From) Home - Tiga",
+  "The Cults - Abducted",
+  "Phoenix - Consolation Prizes",
+  "Harry Chapman - Cats in the Cradle",
+  "Amos Lee - Keep It Loose, Keep It Tight"
+]
+
+def help
+  puts "I accept the following commands:"
+  puts "- help : displays this help message"
+  puts "- list : displays a list of songs you can play"
+  puts "- play : lets you choose a song to play"
+  puts "- exit : exits this program"
+end
+
+def list(songs)
+  songs.each_with_index do |song, place|
+    place = place + 1
+    puts "#{place}. #{song}"
+  end
+end
+
+def play(songs)
+  puts "Please enter a song name or number:"
+  input = gets.chomp
+  puts input.class
+  songs.each_with_index { |song, song_place|
+    puts song.class
+    if input == (song_place + 1).to_s || song.upcase.include?(input.upcase)
+      puts "#{song} is now playing"
+    else
+      puts "Invalid input, please try again"
+    end
+  }
+end
+
+def exit_jukebox
+  puts "Goodbye"
+end
+
+def run(songs)
+  help
+  puts "Please enter a command:"
+  menu_input = gets.chomp
+  
+  while menu_input
+    case menu_input
+    when menu_input == "help"
+      help
+    when menu_input == "list"
+      list(songs)
+    when menu_input == "play"
+      play(songs)
+    when menu_input == "exit"
+      exit_jukebox
+      break
+    end
+  
+  
+end
